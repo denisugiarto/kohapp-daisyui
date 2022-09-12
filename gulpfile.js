20745248576ded4f6f61d596e5619ebace879887
Deni Sugiarto <sugiarto.deni@gmail.com>
2022-09-12 13:43:54 +0700

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,65 @@
+//list dependences
+const { src, dest, watch, series, task } = require('gulp');
+const autoprefixer = require('autoprefixer');
+const minify = require('gulp-clean-css');
+const webp = require('gulp-webp');
+const nunjucksRender = require('gulp-nunjucks-render');
+const ghPages = require('gulp-gh-pages');
+
+//create function
+
+//deploy
+task('deploy', function () {
+  return src('./dist/**/*').pipe(ghPages());
+});
+
+//html
+function htmlTemplate() {
+  return src('src/templates/pages/**/*.{html,njk}')
+    .pipe(nunjucksRender({ path: ['src/templates/components'] }))
+    .pipe(dest('dist'));
+}
+
+//scss
+function compilescss() {
+  return src('src/css/style.css')
+    .pipe(minify())
+    .pipe(dest('dist/assets/css'));
+}
+
+//web images
+function webpImage() {
+  return src('src/images/*.{jpg,png}').pipe(webp()).pipe(dest('dist/assets/images'));
+}
+
+function images() {
+  return src('src/images/*.webp').pipe(dest('dist/assets/images'));
+}
+
+//svg
+function svgCopy() {
+  return src('src/icons/*.svg').pipe(dest('dist/assets/icons'));
+}
+
+//font
+function fontCopy() {
+  return src('src/font/*.*').pipe(dest('dist/assets/font'));
+}
+
+//library
+function libraryCopy() {
+  return src('src/library/**/*.*').pipe(dest('dist/assets/library'));
+}
+
+//create watch task
+function watchTask() {
+  watch('src/templates/**/**/*.html', htmlTemplate);
+  // watch('src/templates/**/**/*.html', compilescss);
+  watch('src/css/style.css', compilescss);
+  watch('src/images/*.{jpg,png}', webpImage);
+  watch('src/images/*.webp', images);
+  watch('src/icons/*.svg', svgCopy);
+}
+
+//default gulp
+exports.default = series(htmlTemplate, compilescss, webpImage, images, svgCopy, fontCopy, libraryCopy, watchTask);
